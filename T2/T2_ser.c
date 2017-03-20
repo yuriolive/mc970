d560c3ee2d635eac2ca806b4a579ab3709120045
Yuri Soares Olive <ra148265@lawrence.lab.ic.unicamp.br>
2017-03-20 16:35:35 -0300

    T2 v0.1

---
+++ T2/T2_ser.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <omp.h>
+
+void producer_consumer(int *buffer, int size, int *vec, int n, int thread_count) {
+	int i, j;
+	long long unsigned int k = 0, sum = 0;
+
+#   pragma omp parallel num_threads(thread_count) default(none) shared(n, size, buffer, vec, sum) private(i, j) 
+	for(i=0;i<n;i++) {
+		if(i % 2 == 0) {	// PRODUTOR
+#           pragma omp for			
+			for(j=0;j<size;j++) {
+				buffer[j] = vec[i] + j*vec[i+1];
+			}
+		} else {	// CONSUMIDOR
+#       pragma omp for
+			for(j=0;j<size;j++) {
+#               pragma omp atomic
+				sum += buffer[j];
+			}
+		}
+	}
+	printf("%llu\n",sum);
+}
+
+int main(int argc, char * argv[]) {
+	double start, end;
+	float d;
+	int i, n, size, nt;
+	int *buff;
+	int *vec;
+
+	scanf("%d %d %d",&nt,&n,&size);
+	
+	buff = (int *)malloc(size*sizeof(int));
+	vec = (int *)malloc(n*sizeof(int));
+
+	for(i=0;i<n;i++)
+		scanf("%d",&vec[i]);
+	
+	start = omp_get_wtime();
+	producer_consumer(buff, size, vec, n, nt);
+	end = omp_get_wtime();
+
+	printf("%lf\n",end-start);
+
+	free(buff);
+	free(vec);
+
+	return 0;
+}
