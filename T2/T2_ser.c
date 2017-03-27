30d6f1829673594806364f9267fc0702978c2e0d
Yuri Soares Olive <ra148265@lawrence.lab.ic.unicamp.br>
2017-03-27 16:17:14 -0300

    T3 bootstrap

--- T2/T2_ser.c
+++ T2/T2_ser.c
@@ -7,15 +7,15 @@ void producer_consumer(int *buffer, int size, int *vec, int n, int thread_count)
 	int i, j;
 	long long unsigned int k = 0, sum = 0;
 
-#   pragma omp parallel num_threads(thread_count) default(none) shared(n, size, buffer, vec) private(i, j) reduction(+: sum)
+//#   pragma omp parallel num_threads(thread_count) default(none) shared(n, size, buffer, vec) private(i, j) reduction(+: sum)
 	for(i=0;i<n;i++) {
 		if(i % 2 == 0) {	// PRODUTOR
-#           pragma omp for			
+//#           pragma omp for			
 			for(j=0;j<size;j++) {
 				buffer[j] = vec[i] + j*vec[i+1];
 			}
 		} else {	// CONSUMIDOR
-#       pragma omp for
+//#       pragma omp for
 			for(j=0;j<size;j++) {
 				sum += buffer[j];
 			}
@@ -50,3 +50,181 @@ int main(int argc, char * argv[]) {
 
 	return 0;
 }
+
+/* 
+
+ * * * * * * * * * * * *
+ * Tarefa Complementar *
+ * * * * * * * * * * * *
+
+* * 
+1- Resultado da execução do comando: cat /proc/cpuinfo
+* *
+
+processor	: 0
+vendor_id	: GenuineIntel
+cpu family	: 6
+model		: 58
+model name	: Intel(R) Core(TM) i5-3570 CPU @ 3.40GHz
+stepping	: 9
+microcode	: 0x1c
+cpu MHz		: 1968.322
+cache size	: 6144 KB
+physical id	: 0
+siblings	: 4
+core id		: 0
+cpu cores	: 4
+apicid		: 0
+initial apicid	: 0
+fpu		: yes
+fpu_exception	: yes
+cpuid level	: 13
+wp		: yes
+flags		: fpu vme de pse tsc msr pae mce cx8 apic sep mtrr pge mca cmov pat pse36 clflush dts acpi mmx fxsr sse sse2 ss ht tm pbe syscall nx rdtscp lm constant_tsc arch_perfmon pebs bts rep_good nopl xtopology nonstop_tsc aperfmperf eagerfpu pni pclmulqdq dtes64 monitor ds_cpl vmx smx est tm2 ssse3 cx16 xtpr pdcm pcid sse4_1 sse4_2 x2apic popcnt tsc_deadline_timer aes xsave avx f16c rdrand lahf_lm epb tpr_shadow vnmi flexpriority ept vpid fsgsbase smep erms xsaveopt dtherm ida arat pln pts
+bugs		:
+bogomips	: 6784.77
+clflush size	: 64
+cache_alignment	: 64
+address sizes	: 36 bits physical, 48 bits virtual
+power management:
+
+processor	: 1
+vendor_id	: GenuineIntel
+cpu family	: 6
+model		: 58
+model name	: Intel(R) Core(TM) i5-3570 CPU @ 3.40GHz
+stepping	: 9
+microcode	: 0x1c
+cpu MHz		: 1872.033
+cache size	: 6144 KB
+physical id	: 0
+siblings	: 4
+core id		: 1
+cpu cores	: 4
+apicid		: 2
+initial apicid	: 2
+fpu		: yes
+fpu_exception	: yes
+cpuid level	: 13
+wp		: yes
+flags		: fpu vme de pse tsc msr pae mce cx8 apic sep mtrr pge mca cmov pat pse36 clflush dts acpi mmx fxsr sse sse2 ss ht tm pbe syscall nx rdtscp lm constant_tsc arch_perfmon pebs bts rep_good nopl xtopology nonstop_tsc aperfmperf eagerfpu pni pclmulqdq dtes64 monitor ds_cpl vmx smx est tm2 ssse3 cx16 xtpr pdcm pcid sse4_1 sse4_2 x2apic popcnt tsc_deadline_timer aes xsave avx f16c rdrand lahf_lm epb tpr_shadow vnmi flexpriority ept vpid fsgsbase smep erms xsaveopt dtherm ida arat pln pts
+bugs		:
+bogomips	: 6788.24
+clflush size	: 64
+cache_alignment	: 64
+address sizes	: 36 bits physical, 48 bits virtual
+power management:
+
+processor	: 2
+vendor_id	: GenuineIntel
+cpu family	: 6
+model		: 58
+model name	: Intel(R) Core(TM) i5-3570 CPU @ 3.40GHz
+stepping	: 9
+microcode	: 0x1c
+cpu MHz		: 2000.280
+cache size	: 6144 KB
+physical id	: 0
+siblings	: 4
+core id		: 2
+cpu cores	: 4
+apicid		: 4
+initial apicid	: 4
+fpu		: yes
+fpu_exception	: yes
+cpuid level	: 13
+wp		: yes
+flags		: fpu vme de pse tsc msr pae mce cx8 apic sep mtrr pge mca cmov pat pse36 clflush dts acpi mmx fxsr sse sse2 ss ht tm pbe syscall nx rdtscp lm constant_tsc arch_perfmon pebs bts rep_good nopl xtopology nonstop_tsc aperfmperf eagerfpu pni pclmulqdq dtes64 monitor ds_cpl vmx smx est tm2 ssse3 cx16 xtpr pdcm pcid sse4_1 sse4_2 x2apic popcnt tsc_deadline_timer aes xsave avx f16c rdrand lahf_lm epb tpr_shadow vnmi flexpriority ept vpid fsgsbase smep erms xsaveopt dtherm ida arat pln pts
+bugs		:
+bogomips	: 6788.63
+clflush size	: 64
+cache_alignment	: 64
+address sizes	: 36 bits physical, 48 bits virtual
+power management:
+
+processor	: 3
+vendor_id	: GenuineIntel
+cpu family	: 6
+model		: 58
+model name	: Intel(R) Core(TM) i5-3570 CPU @ 3.40GHz
+stepping	: 9
+microcode	: 0x1c
+cpu MHz		: 1977.661
+cache size	: 6144 KB
+physical id	: 0
+siblings	: 4
+core id		: 3
+cpu cores	: 4
+apicid		: 6
+initial apicid	: 6
+fpu		: yes
+fpu_exception	: yes
+cpuid level	: 13
+wp		: yes
+flags		: fpu vme de pse tsc msr pae mce cx8 apic sep mtrr pge mca cmov pat pse36 clflush dts acpi mmx fxsr sse sse2 ss ht tm pbe syscall nx rdtscp lm constant_tsc arch_perfmon pebs bts rep_good nopl xtopology nonstop_tsc aperfmperf eagerfpu pni pclmulqdq dtes64 monitor ds_cpl vmx smx est tm2 ssse3 cx16 xtpr pdcm pcid sse4_1 sse4_2 x2apic popcnt tsc_deadline_timer aes xsave avx f16c rdrand lahf_lm epb tpr_shadow vnmi flexpriority ept vpid fsgsbase smep erms xsaveopt dtherm ida arat pln pts
+bugs		:
+bogomips	: 6788.53
+clflush size	: 64
+cache_alignment	: 64
+address sizes	: 36 bits physical, 48 bits virtual
+power management:
+
+
+* * 
+2- Resultado do profile para a execução no caso da entrada 1
+* *
+
+--Serial:
+Each sample counts as 0.01 seconds.
+ no time accumulated
+
+  %   cumulative   self              self     total           
+ time   seconds   seconds    calls  Ts/call  Ts/call  name    
+  0.00      0.00     0.00        1     0.00     0.00  producer_consumer
+
+--Paralelo:
+Each sample counts as 0.01 seconds.
+ no time accumulated
+
+  %   cumulative   self              self     total           
+ time   seconds   seconds    calls  Ts/call  Ts/call  name    
+  0.00      0.00     0.00        1     0.00     0.00  producer_consumer
+
+
+* * 
+3- Resultado do profile para a execução no caso da entrada 2
+* *
+
+--Serial:
+Each sample counts as 0.01 seconds.
+  %   cumulative   self              self     total           
+ time   seconds   seconds    calls  ms/call  ms/call  name    
+100.78      0.29     0.29        1   292.25   292.25  producer_consumer
+
+--Paralelo:
+Each sample counts as 0.01 seconds.
+  %   cumulative   self              self     total           
+ time   seconds   seconds    calls  Ts/call  Ts/call  name    
+100.61      0.30     0.30                             main
+  0.00      0.30     0.00        1     0.00     0.00  producer_consumer
+
+
+* * 
+4- Resultado do profile para  execução no caso da entrada 3
+* *
+
+--Serial:
+Each sample counts as 0.01 seconds.
+  %   cumulative   self              self     total           
+ time   seconds   seconds    calls   s/call   s/call  name    
+100.78      2.48     2.48        1     2.48     2.48  producer_consumer
+
+--Paralelo:
+Each sample counts as 0.01 seconds.
+  %   cumulative   self              self     total           
+ time   seconds   seconds    calls  Ts/call  Ts/call  name    
+100.61      2.25     2.25                             main
+  0.00      2.25     0.00        1     0.00     0.00  producer_consumer
+
+
+*/
---
+++ T3/hist_s.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <float.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+/* funcao que calcula o minimo valor em um vetor */
+double min_val(double * vet,int nval) {
+	int i;
+	double min;
+
+	min = FLT_MAX;
+
+	for(i=0;i<nval;i++) {
+		if(vet[i] < min)
+			min =  vet[i];
+	}
+	
+	return min;
+}
+
+/* funcao que calcula o maximo valor em um vetor */
+double max_val(double * vet, int nval) {
+	int i;
+	double max;
+
+	max = FLT_MIN;
+
+	for(i=0;i<nval;i++) {
+		if(vet[i] > max)
+			max =  vet[i];
+	}
+	
+	return max;
+}
+
+/* conta quantos valores no vetor estao entre o minimo e o maximo passados como parametros */
+int * count(double min, double max, int * vet, int nbins, double h, double * val, int nval) {
+	int i, j, count;
+	double min_t, max_t;
+
+	for(j=0;j<nbins;j++) {
+		count = 0;
+		min_t = min + j*h;
+		max_t = min + (j+1)*h;
+		for(i=0;i<nval;i++) {
+			if(val[i] <= max_t && val[i] > min_t) {
+				count++;
+			}
+		}
+
+		vet[j] = count;
+	}
+
+	return vet;
+}
+
+int main(int argc, char * argv[]) {
+	double h, *val, max, min;
+	int n, nval, i, *vet, size;
+	long unsigned int duracao;
+	struct timeval start, end;
+	pthread_t∗ thread_handles;
+
+    /* numero de threads */
+	scanf("%d",&thread);
+	/* entrada do numero de dados */
+	scanf("%d",&nval);
+	/* numero de barras do histograma a serem calculadas */
+	scanf("%d",&n);
+
+	/* vetor com os dados */
+	val = (double *)malloc(nval*sizeof(double));
+	vet = (int *)malloc(n*sizeof(int));
+
+	/* entrada dos dados */
+	for(i=0;i<nval;i++) {
+		scanf("%lf",&val[i]);
+	}
+
+	/* calcula o minimo e o maximo valores inteiros */
+	min = floor(min_val(val,nval));
+	max = ceil(max_val(val,nval));
+
+	/* calcula o tamanho de cada barra */
+	h = (max - min)/n;
+
+	gettimeofday(&start, NULL);
+
+
+
+	/* chama a funcao */
+	vet = count(min, max, vet, n, h, val, nval);
+
+
+
+	gettimeofday(&end, NULL);
+
+	duracao = ((end.tv_sec * 1000000 + end.tv_usec) - \
+	(start.tv_sec * 1000000 + start.tv_usec));
+
+	printf("%.2lf",min);	
+	for(i=1;i<=n;i++) {
+		printf(" %.2lf",min + h*i);
+	}
+	printf("\n");
+
+	/* imprime o histograma calculado */	
+	printf("%d",vet[0]);
+	for(i=1;i<n;i++) {
+		printf(" %d",vet[i]);
+	}
+	printf("\n");
+
+	/* imprime o tempo de duracao do calculo */
+	printf("%lu\n",duracao);
+
+	free(vet);
+	free(val);
+
+	return 0;
+}
