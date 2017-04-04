c4f2096ead5b4b574660b01e0351f7423e7e8277
Yuri Soares Olive <ra148265@zecolmeia.lab.ic.unicamp.br>
2017-04-04 11:03:01 -0300

    final

--- T4/pi_s_p.c
+++ T4/pi_s_p.c
@@ -1,5 +1,3 @@
-#define __USE_POSIX
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -7,8 +5,7 @@
 #include <pthread.h>
 
 typedef struct {
-    unsigned int sval;
-    unsigned int fval;
+    unsigned int nval;
     unsigned int in;
 } Args;
 
@@ -17,23 +14,26 @@ long unsigned int thread_count;
 
 void * monte_carlo_pi_parallel(void * count_args) {
     Args * args = (Args *) count_args;
-	long long unsigned int i;
+	long long unsigned int i, in = 0;
 	double x, y, d;
-
-	for (i = args->sval; i <= args->fval; i++) {
-		x = ((rand_r(&(args->sval)) % 1000000)/500000.0)-1;
-		y = ((rand_r(&(args->sval)) % 1000000)/500000.0)-1;
+	unsigned int seed = time(NULL);
+    
+	for (i = 0; i < args->nval; i++) {
+		x = ((rand_r(&seed) % 1000000)/500000.0)-1;
+		y = ((rand_r(&seed) % 1000000)/500000.0)-1;
 		d = ((x*x) + (y*y));
 		if (d <= 1) {
-		    args->in += 1;
+		    in++;
 		}
 	}
+	
+	args->in = in;
 	
 	return NULL;
 }
 
 int main(void) {
-	unsigned int nval, i, group;
+	unsigned int nval, i;
 	double pi;
 	long unsigned int duracao;
 	long long unsigned int in = 0;
@@ -45,22 +45,18 @@ int main(void) {
     thread_handles = (pthread_t*)malloc(thread_count*sizeof(pthread_t));
     count_args = (Args *)malloc(thread_count*sizeof(Args));
 
-    /* grupo de valores a ser executado por cada threads */
-    group = floor(nval/thread_count);
-
 	srand (time(NULL));
 
 	gettimeofday(&start, NULL);
 	
 	/* cria as threads */
-    for(i = 0; i < thread_count - 1; i++) {
-    	count_args[i].sval = i*group;
-        count_args[i].fval = (i+1)*group - 1;
-        pthread_create(&thread_handles[i], NULL, monte_carlo_pi_parallel, &count_args[i]);
+    for(i = 0; i < thread_count; i++) {
+        count_args[i].nval = nval/thread_count;
+        if(pthread_create(&thread_handles[i], NULL, monte_carlo_pi_parallel, (void *) &count_args[i])) {
+            printf("Erro ao criar thread\n");
+            exit(1);
+        };
     }
-	count_args[i].sval = i*group;
-	count_args[i].fval = (i+1)*group - 1 + nval%thread_count;
-    pthread_create(&thread_handles[i], NULL, monte_carlo_pi_parallel, &count_args[i]);
     
     for(i = 0; i < thread_count; i++) {
         pthread_join(thread_handles[i], NULL);
@@ -73,7 +69,6 @@ int main(void) {
     
 	pi = 4*in/((double)nval);
 	printf("%lf\n%lu\n",pi,duracao);
-	
 	
     free(thread_handles);
     pthread_exit(NULL);    
